cfe646ee4002757c54daefc921f8399b696fcbf2
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 09:05:56 -0500

    impirmi el numero de cadenas con ayuda de len y split

--- ejericico1.py
+++ ejericico1.py
@@ -2,4 +2,5 @@
 "Ejericico numero 1 "
 cadena = "Mi nombre es joaquin lopez y soy estudiante de ingenieria de sistemas"
 print("Mi nombre es joaquin lopez y soy estudiante de ingenieria de sistemas ")
+print(f"El numero de palabras de la cadena es : {len(cadena.split())}")
 
