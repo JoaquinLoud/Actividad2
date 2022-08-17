e012bacc06327c5d293627b394318a66c8b40432
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 10:19:03 -0500

    creamos variable numero que se repite y colocamos la funcion para que calcule el numero de veces que se repite una palabra

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,3 +2,4 @@
 #Leer una cadena y devolver cada palabra de la cadena y el nuemero de veces que se repite
 cadena = "bases de datos: es tipo de carpeta de datos "
 substring = "datos"
+numeroqueserepite = cadena.count(substring)
