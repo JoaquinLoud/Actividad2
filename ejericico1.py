787dc68eabf59204629e94aa1dc52f5ea3ad14ee
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 09:24:31 -0500

    Detalle algunas lineas de codigo

--- ejericico1.py
+++ ejericico1.py
@@ -1,6 +1,8 @@
 
-"Ejericico numero 1 "
+#Ejericico numero 1
 cadena = "Mi nombre es joaquin lopez y soy estudiante de ingenieria de sistemas"
 print("Mi nombre es joaquin lopez y soy estudiante de ingenieria de sistemas ")
 print(f"El numero de palabras de la cadena es : {len(cadena.split())}")
+
+#Ejericico numero 2
 
