d1d9bf31b1913bf8c87cc6943124d5d66e1767a0
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 11:00:50 -0500

    imprim cadena para que muestre el contenido de la cadena

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,6 @@
 #Ejericico 2
 #Leer una cadena y devolver cada palabra de la cadena y el nuemero de veces que se repite
-cadena = "Desarrollo de Software: Desarrollo"
+cadena = "Desarrollo de Software: Desarrollo \n"
+print(cadena)
+
+
