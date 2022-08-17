0098e60c2eaaad3260b7f1dea335a1fab1941451
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 09:24:59 -0500

    detalle alunas lineas

--- ejericico1.py
+++ ejericico1.py
@@ -1,8 +1,5 @@
-
 #Ejericico numero 1
+#Leer una cadena y devolver el numero de palabras de dicha cadena
 cadena = "Mi nombre es joaquin lopez y soy estudiante de ingenieria de sistemas"
 print("Mi nombre es joaquin lopez y soy estudiante de ingenieria de sistemas ")
 print(f"El numero de palabras de la cadena es : {len(cadena.split())}")
-
-#Ejericico numero 2
-
