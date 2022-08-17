868833aeaf03701f0067984ee4c201da446830e0
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 10:54:53 -0500

    borre el ejericio porque no cumplia con lo requerido

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,3 @@
 #Ejericico 2
 #Leer una cadena y devolver cada palabra de la cadena y el nuemero de veces que se repite
-cadena = "bases de datos: es tipo de carpeta de datos "
-substring = "datos"
-numeroqueserepite = cadena.count(substring)
-print(f"la palabra que se repite en la cadena es: {substring} y el numero de veces que se repite es:{numeroqueserepite} gracias")
+cadena = "Desarrollo de Software: Desarrollo"
