65c1070a88b43f1f29a7db708b355a4c679b3ad0
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 17:42:28 -0500

    .

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,1 +1,6 @@
-Lista_uno = [ 17 , 8 , 2022 ,"Grupo e.bases de datos" , True  ,["programa" , "ingenieria de sistemas" , "semestre" , 9]  ] 
+# Lista_uno = [ 17 , 8 , 2022 ,"Grupo e.bases de datos" , True  ,["programa" , "ingenieria de sistemas" , "semestre" , 9]  ] 
+listaConElementos = [30, 2000000,"Jaider Quintero", "Docente", True, ["Magister", "Doctorado", "Especializacion", 20]]
+
+for x in listaConElementos[5]:
+    print(x)
+    
