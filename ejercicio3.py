3ad57884462f4f5127594e023b48184978758c47
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 20:02:44 -0500

    indexamos

--- ejercicio3.py
+++ ejercicio3.py
@@ -2,12 +2,12 @@ listauno=[]
 terminar=True
 while(terminar):
      nombre = input("Ingrese el Nombre > ")
-    apellido = input("Ingrese el Apellido > ")
-    edad = int(input("Ingrese la Edad > "))
-    listauno.append([nombre, apellido, edad])
-    t = input("Desea terminar S/N")
-    if(t == "n"):
-        terminar = False
+     apellido = input("Ingrese el Apellido > ")
+     edad = int(input("Ingrese la Edad > "))
+     listauno.append([nombre, apellido, edad])
+     t = input("Desea terminar S/N")
+     if(t == "n"):
+      terminar = False
 print(listauno)
 listados=listauno
 listados.sort()
