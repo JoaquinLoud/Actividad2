f69845fad30d57c7e0822a6ec64b4f57ae88d6bb
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 19:57:01 -0500

    agregamos el append para que nos muestre los valores que agrego el usuario

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,3 +4,4 @@ while(terminar):
      nombre = input("Ingrese el Nombre > ")
     apellido = input("Ingrese el Apellido > ")
     edad = int(input("Ingrese la Edad > "))
+    listauno.append([nombre, apellido, edad])
