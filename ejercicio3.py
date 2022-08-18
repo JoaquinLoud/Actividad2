b44949f7b5cb8e3fc15881d5a7b1b694fcdcf72c
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 19:56:21 -0500

    agregamos los input para que el usuario dijite

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,3 +1,6 @@
 listauno=[]
 terminar=True
 while(terminar):
+     nombre = input("Ingrese el Nombre > ")
+    apellido = input("Ingrese el Apellido > ")
+    edad = int(input("Ingrese la Edad > "))
