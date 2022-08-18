04392e2e08ce9ec4e9eb4c9c5a4ec2e8facfc991
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 19:57:49 -0500

    preguntamos si desea terminar

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,3 +5,5 @@ while(terminar):
     apellido = input("Ingrese el Apellido > ")
     edad = int(input("Ingrese la Edad > "))
     listauno.append([nombre, apellido, edad])
+    t = input("Desea Continuar S/N")
+    if(t == "n"):
