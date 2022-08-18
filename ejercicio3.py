2030943295f838347dd5b877844db39b37f2c3dd
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-17 19:52:03 -0500

    modifique una linea

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,6 +1,2 @@
-# Lista_uno = [ 17 , 8 , 2022 ,"Grupo e.bases de datos" , True  ,["programa" , "ingenieria de sistemas" , "semestre" , 9]  ] 
-listaConElementos = [30, 2000000,"Jaider Quintero", "Docente", True, ["Magister", "Doctorado", "Especializacion", 20]]
-
-for x in listaConElementos[5]:
-    print(x)
-    
+Lista_uno = [ 17 , 8 , 2022 ,"Grupo e.bases de datos" , True  ,["programa" , "ingenieria de sistemas" , "semestre" , 9]  ] 
+print(Lista_uno)
